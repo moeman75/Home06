5816714eb0d6f769c10544b17aa5a44252846027
Grant Moe <grantmoe2@gmail.com>
2014-11-21 14:27:23 -0600

    TrainTest

---
+++ src/home06/TrainTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package home06;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gmoe
+ */
+public class TrainTest 
+{
+    Train train = new Train(); 
+    Scanner scan = new Scanner("passengers");
+    
+}
